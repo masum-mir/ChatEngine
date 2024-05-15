a9ade0e967ebe8501d73ab5e1e26e24e93cff2b4
masum <masummir.lm@gmail.com>
2024-05-15 21:46:10 +0600

    added

--- src/Client/chat/Client.java
+++ src/Client/chat/Client.java
@@ -5,21 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Client {
 
+    static final String ipAddress = "localhost";
+    static final int port = 8080;
     private Socket socket;
     private BufferedReader input;
     private PrintWriter out;
     private BufferedReader in;
+    
+    Map<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
-        Client client = new Client("localhost", 8080);
+
+        Client client = new Client(ipAddress, port); 
     }
 
     public Client(String address, int port) {
         try {
-            // Connect to the chat server
+           
             socket = new Socket(address, port);
             System.out.println("Connected to the chat server");
 
@@ -41,6 +48,7 @@ public class Client {
             String userInput;
             while (true) {
                 userInput = input.readLine();
+
                 out.println(userInput);
             }
 
--- src/Server/chat/Server.java
+++ src/Server/chat/Server.java
@@ -3,21 +3,23 @@ package Server.chat;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import Client.chat.User;
 
-public class Server extends javax.swing.JFrame {
+public class Server {
 
     private static List<ClientHandler> clients = new ArrayList<>();
+    private static Map<String, User> users = new HashMap<>();
+    private static int port = 8080;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
+        ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server started. Waiting for clients...");
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected: " + clientSocket);
 
-            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
-            clients.add(clientThread);
+            ClientHandler clientThread = new ClientHandler(clientSocket, clients, users);
             new Thread(clientThread).start();
         }
     }
@@ -27,12 +29,15 @@ class ClientHandler implements Runnable {
 
     private Socket clientSocket;
     private List<ClientHandler> clients;
+    private Map<String, User> users;
     private PrintWriter out;
     private BufferedReader in;
+    private String userName;
 
-    public ClientHandler(Socket socket, List<ClientHandler> clients) throws IOException {
+    public ClientHandler(Socket socket, List<ClientHandler> clients, Map<String, User> users) throws IOException {
         this.clientSocket = socket;
         this.clients = clients;
+        this.users = users;
         this.out = new PrintWriter(clientSocket.getOutputStream(), true);
         this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
@@ -40,11 +45,26 @@ class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
+            while (true) {
+                out.println("Enter 1 to Register, 2 to Login:");
+                String option = in.readLine();
+
+                if ("1".equals(option)) {
+                    registerUser();
+                } else if ("2".equals(option)) {
+                    if (loginUser()) {
+                        break;
+                    }
+                }
+            }
+
+            clients.add(this);
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-
                 for (ClientHandler client : clients) {
-                    client.out.println(inputLine);
+                    if (client != this) {
+                        client.out.println(users.get(userName) + ": " + inputLine);
+                    }
                 }
             }
         } catch (IOException e) {
@@ -56,7 +76,49 @@ class ClientHandler implements Runnable {
                 clientSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                clients.remove(this);
+                System.out.println("Client disconnected: " + clientSocket);
             }
         }
     }
+
+    private void registerUser() throws IOException {
+        out.println("Enter username:");
+        String username = in.readLine();
+        out.println("Enter email:");
+        String email = in.readLine();
+        out.println("Enter phone number:");
+        String phoneNumber = in.readLine();
+        out.println("Enter password:");
+        String password = in.readLine();
+
+        if (users.containsKey(username)) {
+            System.out.println("Username already exists. Try again.");
+        } else {
+            User user = new User(System.currentTimeMillis(), username, email, phoneNumber, password, true);
+            users.put(username, user);
+            System.out.println("Registration successful.");
+        }
+
+    }
+
+    private boolean loginUser() throws IOException {
+        out.println("Enter username:");
+        String username = in.readLine();
+        out.println("Enter password:");
+        String password = in.readLine();
+
+        User user = users.get(username);
+        if (user != null && password.equals(user.getPassword())) {
+            out.println("Login successful.");
+            userName = username;
+            return true;
+        } else {
+            out.println("Invalid username or password. Try again.");
+            return false;
+        }
+
+    }
+
 }
